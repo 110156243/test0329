4f32b1c6ef4d3e5b0a323cf0db782030daf68fa1
C110156243 <c110156243@nkust.edu.tw>
2022-06-22 17:10:36 +0800

    main

--- test01.py
+++ test01.py
@@ -1,1 +1,18 @@
-print("abc")
+a = input("請輸入正整數 : ")
+res = []
+for i in range(0,len(a)):
+    for j in range(i+1):
+        say = int(a[j:len(a)-i+j])
+        if say % 2 == 1 :
+            sayl = []
+            for k in range(1,say+1):
+                if say % k == 0 :
+                    sayl.append(k)
+            if len(sayl) == 2 : res.append(say)
+
+if len(res) == 0:
+    print("子字串中最大的質數是 : No prime found")
+elif len(res) == 1 and 1 in res:
+    print("子字串中最大的質數是 : No prime found")
+else:
+    print("子字串中最大的質數是 : ",max(res))
