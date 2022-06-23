30809a3e258d0cbdd4ee6000a15ac1d361dfa956
C110156243 <c110156243@nkust.edu.tw>
2022-06-23 23:32:44 +0800

    main

---
+++ test50.py
@@ -0,0 +1,6 @@
+all=set(['John','Mary','Tina','Fiona','Claire','Eva','Ben','Bill','Bert'])
+english=set(['John','Mary','Fiona','Claire','Ben','Bill'])
+math=set(['Mary','Fiona','Claire','Eva','Ben'])
+print("英文與數學都及格"+" "+str(math&english))
+print("數學不及格"+" "+str(all-math))
+print("英文及格且數學不及格"+" "+str(english&(all-math)))
